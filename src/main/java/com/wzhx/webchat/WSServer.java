aac708b5c7352cc1b3c2c0bc3b6093e06c9ee8da
JAWZHX <1219251709@qq.com>
2018-10-19 11:02:16 +0800

    1、创建WSServer类（websocket服务器）2、TODO：定义子处理器（channel的初始化处理器）

---
+++ src/main/java/com/wzhx/webchat/WSServer.java
@@ -0,0 +1,34 @@
+package com.wzhx.webchat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 这是一个websocket服务器
+ * 
+ * @author 12192
+ *
+ */
+public class WSServer {
+
+	public static void main(String[] args) throws Exception {
+//		1、创建主从线程池组
+		EventLoopGroup mainGroup = new NioEventLoopGroup();
+		EventLoopGroup subGroup = new NioEventLoopGroup();
+
+//		2、创建服务器启动类
+		try {
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(mainGroup, subGroup).channel(NioServerSocketChannel.class).childHandler(null);
+			ChannelFuture channelFuture = serverBootstrap.bind(8080).sync();
+			channelFuture.channel().closeFuture();
+		} finally {
+			mainGroup.shutdownGracefully();
+			subGroup.shutdownGracefully();
+		}
+	}
+
+}
